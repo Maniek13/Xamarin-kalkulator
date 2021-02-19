2e57f0d030c949e995f9ff42252fa53a3f5a1c93
Maniek13 <mani3k1989@gmail.com>
2021-02-19 01:24:52 +0100

    kom

---
+++ Calc/Calc.cs
@@ -0,0 +1,161 @@
+﻿using System.Text;
+using System;
+
+
+namespace Core
+{
+
+    public class Calc
+    { 
+
+        public static string Policz()
+        {
+            double wynik = 0;
+            double liczba1, liczba2;
+            string[] liczby = Phoneword.MainActivity.liczba;
+
+            if (liczby[0] == "")
+            {
+                liczba1 = 0;
+            }
+            else if (liczby[0] == "." || liczby[0] == "-." || liczby[0] == "-")
+            {
+                liczba1 = 0;
+            }
+            else
+            {
+                liczba1 = Convert.ToDouble(liczby[0]);
+            }
+
+
+            if (Phoneword.MainActivity.liczba[1] == "")
+            {
+                liczba2 = 0;
+            }
+            else if (liczby[1] == "." || liczby[1] == "-." || liczby[1] == "-")
+            {
+                liczba2 = 0;
+            }
+            else
+            {
+                liczba2 = Convert.ToDouble(liczby[1]);
+            }
+
+
+            if (Phoneword.MainActivity.znak == "+")
+            {
+                wynik = liczba1 + liczba2;
+            }
+            else if (Phoneword.MainActivity.znak == "-")
+            {
+                wynik = liczba1 - liczba2;
+            }
+            else if (Phoneword.MainActivity.znak == "/")
+            {
+                if(liczba2 != 0)
+                {
+                    wynik = liczba1 / liczba2;
+                }
+                else
+                {
+                    return "Nie można dzielić przez zero";
+                }
+                   
+            }
+            else if (Phoneword.MainActivity.znak == "*")
+            {
+                wynik = liczba1 * liczba2;
+
+            }
+
+            Phoneword.MainActivity.liczba[0] = wynik.ToString();
+            Phoneword.MainActivity.liczba[1] = "";
+            Phoneword.MainActivity.znak = "";
+
+            if(wynik > 0)
+            {
+                Phoneword.MainActivity.zmiana = false;
+            }
+            else
+            {
+                Phoneword.MainActivity.zmiana = true;
+            }
+
+            if(wynik - Math.Truncate(wynik) > 0)
+            {
+                Phoneword.MainActivity.przecinek_zmienna = true;
+            }
+            else
+            {
+                Phoneword.MainActivity.przecinek_zmienna = false;
+            }
+
+            return wynik.ToString();
+        }
+
+        public static string wynik_drukuj(string temp)
+        {
+            if (string.IsNullOrWhiteSpace(temp))
+            {
+                return string.Empty;
+            }
+            else
+            {
+                return temp;
+            }
+        }
+
+        public static string Działanie(string temp)
+        {
+            Phoneword.MainActivity.przecinek_zmienna = false;
+            Phoneword.MainActivity.zmiana = false;
+            return temp;
+        }
+
+        public static string Liczba(string temp, string[] liczba, string znak)
+        {
+            string wynik;
+            if (znak == "")
+            {
+                wynik = liczba[0] += temp;
+            }
+            else
+            {
+                wynik = liczba[1] += temp;
+            }
+            return wynik;
+        }
+
+        public static string Zmiana_znaku(string[] liczba, string znak, bool znak_zmiana)
+        {
+            string temp;
+            if (znak == "")
+            {
+               if(znak_zmiana == true)
+                {
+                    liczba[0] = "-" + liczba[0];
+                }
+                else
+                {
+                    liczba[0] = liczba[0].Substring(1);
+                }
+                temp = liczba[0];
+            }
+            else
+            {
+                if (znak_zmiana == true)
+                {
+                    liczba[1] = "-" + liczba[1];
+                }
+                else
+                {
+                    liczba[1] = liczba[0].Substring(1);
+                }
+
+                temp = liczba[1];
+            }
+
+            return temp;
+        }
+    }
+}
---
+++ Calc/MainActivity.cs
@@ -0,0 +1,223 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace Phoneword
+{
+    [Activity(Label = "Kalkulator", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        public static string[] liczba = { "", ""};
+        public static string znak = "";
+        public static bool przecinek_zmienna = false;
+        public static bool zmiana = false;
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+
+            SetContentView(Calc.Resource.Layout.activity_main);
+
+
+            TextView wynik_field = FindViewById<TextView>(Calc.Resource.Id.wynik_field);
+            Button btn0 = FindViewById<Button>(Calc.Resource.Id.btn0);
+            Button btn1 = FindViewById<Button>(Calc.Resource.Id.btn1);
+            Button btn2 = FindViewById<Button>(Calc.Resource.Id.btn2);
+            Button btn3 = FindViewById<Button>(Calc.Resource.Id.btn3);
+            Button btn4 = FindViewById<Button>(Calc.Resource.Id.btn4);
+            Button btn5 = FindViewById<Button>(Calc.Resource.Id.btn5);
+            Button btn6 = FindViewById<Button>(Calc.Resource.Id.btn6);
+            Button btn7 = FindViewById<Button>(Calc.Resource.Id.btn7);
+            Button btn8 = FindViewById<Button>(Calc.Resource.Id.btn8);
+            Button btn9 = FindViewById<Button>(Calc.Resource.Id.btn9);
+            Button btnPlus = FindViewById<Button>(Calc.Resource.Id.btnPlus);
+            Button btnMinus = FindViewById<Button>(Calc.Resource.Id.btnMinus);
+            Button btnRazy = FindViewById<Button>(Calc.Resource.Id.btnRazy);
+            Button btnPodzielic = FindViewById<Button>(Calc.Resource.Id.btnPodzielic);
+            Button wynik = FindViewById<Button>(Calc.Resource.Id.wynik);
+            Button kasuj = FindViewById<Button>(Calc.Resource.Id.btnKasuj);
+            Button znak_zmiana = FindViewById<Button>(Calc.Resource.Id.btnZnak);
+            Button przecinek = FindViewById<Button>(Calc.Resource.Id.btnPrzecinek);
+            Button delete = FindViewById<Button>(Calc.Resource.Id.btndelete);
+
+            wynik_field.Text = "0";
+
+
+            btn0.Click += (sender, e) =>
+            {
+                wynik_field.Text = Core.Calc.wynik_drukuj(Core.Calc.Liczba("0", liczba, znak).ToString());
+            };
+
+            btn1.Click += (sender, e) =>
+            {
+                wynik_field.Text = Core.Calc.wynik_drukuj(Core.Calc.Liczba("1", liczba, znak).ToString());
+            };
+
+            btn2.Click += (sender, e) =>
+            {
+                wynik_field.Text = Core.Calc.wynik_drukuj(Core.Calc.Liczba("2", liczba, znak).ToString());
+            };
+
+            btn3.Click += (sender, e) =>
+            {
+                wynik_field.Text = Core.Calc.wynik_drukuj(Core.Calc.Liczba("3", liczba, znak).ToString());
+            };
+
+            btn4.Click += (sender, e) =>
+            {
+                wynik_field.Text = Core.Calc.wynik_drukuj(Core.Calc.Liczba("4", liczba, znak).ToString());
+            };
+
+            btn5.Click += (sender, e) =>
+            {
+                wynik_field.Text = Core.Calc.wynik_drukuj(Core.Calc.Liczba("5", liczba, znak).ToString());
+            };
+
+            btn6.Click += (sender, e) =>
+            {
+                wynik_field.Text = Core.Calc.wynik_drukuj(Core.Calc.Liczba("6", liczba, znak).ToString());
+            };
+
+            btn7.Click += (sender, e) =>
+            {
+                wynik_field.Text = Core.Calc.wynik_drukuj(Core.Calc.Liczba("7", liczba, znak).ToString());
+            };
+
+            btn8.Click += (sender, e) =>
+            {
+                wynik_field.Text = Core.Calc.wynik_drukuj(Core.Calc.Liczba("8", liczba, znak).ToString());
+            };
+
+            btn9.Click += (sender, e) =>
+            {
+                wynik_field.Text = Core.Calc.wynik_drukuj(Core.Calc.Liczba("9", liczba, znak).ToString());
+            };
+
+
+            btnPlus.Click += (sender, e) =>
+            {
+                if (znak == "")
+                {
+                    znak = Core.Calc.Działanie("+");
+                }
+                else
+                {
+                    wynik_field.Text = Wykonaj();
+                }
+            };
+
+            btnMinus.Click += (sender, e) =>
+            {
+                if (znak == "")
+                {
+                    znak = Core.Calc.Działanie("-");
+                }
+                else
+                {
+                    wynik_field.Text = Wykonaj();
+                }
+            };
+
+            btnPodzielic.Click += (sender, e) =>
+            {
+                if (znak == "")
+                {
+                    znak = Core.Calc.Działanie("/");
+                }
+                else
+                {
+                    wynik_field.Text = Wykonaj();
+                }
+            };
+
+            btnRazy.Click += (sender, e) =>
+            {
+                if (znak == "")
+                {
+                    znak = Core.Calc.Działanie("*");
+                }
+                else
+                {
+                    wynik_field.Text = Wykonaj();
+                }
+            };
+
+            wynik.Click += (sender, e) =>
+            {
+                wynik_field.Text = Wykonaj();
+            };
+
+
+            kasuj.Click += (sender, e) =>
+            {
+                liczba[0] = "";
+                liczba[1] = "";
+                znak = "";
+                przecinek_zmienna = false;
+                zmiana = false;
+                wynik_field.Text = "0";
+            };
+
+            znak_zmiana.Click += (sender, e) =>
+            {
+                if(zmiana == false)
+                {
+                    zmiana = true;
+                }
+                else
+                {
+                    zmiana = false;
+                }
+
+                wynik_field.Text = Core.Calc.wynik_drukuj(Core.Calc.Zmiana_znaku(liczba, znak, zmiana).ToString());
+            };
+
+            przecinek.Click += (sender, e) =>
+            {
+                if(przecinek_zmienna == false)
+                {
+                    wynik_field.Text = Core.Calc.wynik_drukuj(Core.Calc.Liczba(".", liczba, znak).ToString());
+                    przecinek_zmienna = true;
+                }
+            };
+
+            delete.Click += (sender, e) =>
+            {
+                if (znak == "")
+                {
+                    if(liczba[0].Length > 0)
+                    {
+                        liczba[0] = liczba[0].Substring(0, liczba[0].Length-1);
+                        wynik_field.Text = liczba[0];
+                    }
+                    if(liczba[0].Length == 0 && przecinek_zmienna == true)
+                    {
+                        przecinek_zmienna = false;
+                    }
+                }
+                else
+                {
+                    if(liczba[1].Length > 0)
+                    {
+                        liczba[1] = liczba[1].Substring(0, liczba[1].Length - 1);
+                        wynik_field.Text = liczba[1];
+                    }
+                    if (liczba[1].Length == 0 && przecinek_zmienna == true)
+                    {
+                        przecinek_zmienna = false;
+                    }
+                }
+            };
+        }
+
+        private static string Wykonaj()
+        {
+            string temp = Core.Calc.Policz();
+            return Core.Calc.wynik_drukuj(temp);
+        }
+    }
+}
